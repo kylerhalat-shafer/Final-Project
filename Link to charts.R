a33c2f495e28bb554af38c286349929cf62eedb6
Kyler Halat-shafer <kh5967a@american.edu>
2017-04-27 17:04:04 -0400

    Final Project

---
+++ Link to charts.R
@@ -0,0 +1,17 @@
+##Below is a link to my plot.ly account
+##There you will find graphics for my project 
+
+## "Financial Scatter"is comparative time series plots to show a created
+## index that I made with other REITs over the past 2 and half years or
+## so
+
+##"Pandas-polar-chart" is an adaptation of a polar chart, where the blue
+##line is a moving average from the same created index and the scatter
+## (pink dots) are a stock price. The time period is 360 days -- which 
+## does not include weekends or holidays, so it is a bit more than a year 
+## in the trading world 
+
+##Other graphs and datasets were done in the process of me figuring out
+##how to make the former two graphs 
+
+https://plot.ly/~kyler.halatshafer
